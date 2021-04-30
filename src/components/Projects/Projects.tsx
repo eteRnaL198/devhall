2a3f0e21a09cd5921cdbdd64aa19fe8f7797a625
eternal198 <ikuya313198@gmail.com>
2021-04-30 21:40:40 +0900

    update

--- src/components/Projects/Projects.tsx
+++ src/components/Projects/Projects.tsx
@@ -5,27 +5,36 @@ type Props = {
 const Projects: React.FC<Props> = ({ mainScreen }) => {
   return (
     (mainScreen !== "Projects") ? null :
-    <div className="w-full">
-      <div className="flex justify-around mx-3 my-3">
-        <button
-          className="bg-white flex-1 font-bold mx-2 px-2 rounded-full"
-          >進行中
-        </button>
-        <button
-          className="bg-white flex-1 font-bold mx-2 px-2 rounded-full"
-          >募集中
-        </button>
-        <button
-          className="bg-white flex-1 font-bold mx-2 px-2 rounded-full"
-          >応募済
-        </button>
-        <button
-          className="bg-white flex-1 font-bold mx-2 px-2 rounded-full"
-          >いいね
-        </button>
+    <>
+      <button
+        className="bg-white flex-1 font-bold mx-2 px-2 rounded-full"
+        >作成
+      </button>
+      <div className="w-full">
+        <div className="flex justify-around mx-3 my-3 overflow-x-scroll">
+          <button
+            className="bg-white flex-1 font-bold mx-2 px-2 rounded-full"
+            >探す
+          </button>
+          <button
+            className="bg-white flex-1 font-bold mx-2 px-2 rounded-full"
+            >進行中
+          </button>
+          <button
+            className="bg-white flex-1 font-bold mx-2 px-2 rounded-full"
+            >募集中
+          </button>
+          <button
+            className="bg-white flex-1 font-bold mx-2 px-2 rounded-full"
+            >応募済
+          </button>
+          <button
+            className="bg-white flex-1 font-bold mx-2 px-2 rounded-full"
+            >いいね
+          </button>
+        </div>
       </div>
-      {/* <> */}
-    </div>
+    </>
   )
 }
 
