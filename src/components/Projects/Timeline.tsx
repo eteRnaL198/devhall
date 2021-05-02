3825c136cefa314857fdb17349f62b00119dd149
eternal198 <ikuya313198@gmail.com>
2021-05-02 16:22:41 +0900

    update projects

--- src/components/Projects/Timeline.tsx
+++ src/components/Projects/Timeline.tsx
@@ -2,12 +2,16 @@ type Props = {
   timeline: string;
 }
 
-type Button = {
-  label: 'like',
-  name: 'いいね',
-  liked: string,
+type A = {
+  label: string,
+  name: string,
+  count: number,
 }
-// 探す {
+type B = {
+  label: string,
+}
+type C = A | B;
+// 探す, 募集中， 応募済み， いいね {
 //   詳細
 //   いいね
 //   応募
@@ -17,21 +21,6 @@ type Button = {
 //   ボード
 //   メッセージ
 // }
-// 募集中 {
-//   詳細 > 編集
-//   いいね
-//   応募
-// }
-// 応募済み {
-//   詳細
-//   いいね
-//   応募
-// }
-// いいね {
-//   詳細
-//   いいね
-//   応募
-// }
 
 const Timeline: React.FC<Props> = ({ timeline }) => {
   return (
@@ -40,19 +29,26 @@ const Timeline: React.FC<Props> = ({ timeline }) => {
         <p className="font-bold text-xl">Todoアプリ開発</p>
         <p className="mb-4">React, TypeScriptの練習</p>
         <div className="flex justify-around">
-          <div className="flex text-xs w-1/3">
+          <div className="flex text-xs w-1/4">
+            <button className="border-2 font-bold flex-grow  py-1 rounded-full"
+            >詳細
+            </button>
+          </div>
+          <div className="flex text-xs w-1/4">
             <button className="border-2 font-bold flex-grow  py-1 rounded-full"
             >いいね
             </button>
             <span className="ml-2 py-1"
-            >20</span>
+            >20
+            </span>
           </div>
-          <div className="flex text-xs w-1/3">
-            <button className="border-2 font-bold flex-grow py-1 rounded-full"
+          <div className="flex text-xs w-1/4">
+            <button className="border-2 font-bold flex-grow  py-1 rounded-full"
             >応募
             </button>
             <span className="ml-2 py-1"
-            >3</span>
+            >3
+            </span>
           </div>
         </div>
       </div>
